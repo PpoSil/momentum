a90e9c8d65f82bbd81aa417637e9ec0c44ffcce4
PpoSil <caca30@naver.com>
2024-05-25 11:45:32 +0000

    delete a todo

--- js/todo.js
+++ js/todo.js
@@ -2,15 +2,27 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
+function deleteTodo(e) {
+  const li = e.target.parentElement;
+  li.remove();
+}
+
 function paintTodo(toDo) {
   // li와 span 생성
   const todoLi = document.createElement("li");
   const todoSpan = document.createElement("span");
+  const todoBtn = document.createElement("button");
+
+  todoSpan.innerText = toDo;
+  todoBtn.innerText = "❌";
+
+  todoBtn.addEventListener("click", deleteTodo);
 
   // li 안에 span 넣기
   todoLi.appendChild(todoSpan);
-  todoSpan.innerText = toDo;
+  todoLi.appendChild(todoBtn);
 
+  // todoList안에 li넣기
   todoList.appendChild(todoLi);
 }
 
