eee3748b7c88bdc399b4c65403cae4f0d17c856b
PpoSil <caca30@naver.com>
2024-05-25 10:53:37 +0000

    add a background image

---
+++ js/backgorund.js
@@ -0,0 +1,13 @@
+const images = ["Seoul.jpg", "Paris.jpg", "Greece.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+// createElement() -> html 요소 생성
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+console.log(bgImage);
+
+// appendChild() -> body에 html 요소 추가
+document.body.appendChild(bgImage);
